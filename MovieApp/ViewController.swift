d3744f2c0eac6e1a146fe96dfbb183538345eebf
Antun Herkov <antunherkov@gmail.com>
2022-03-28 17:20:14 +0200

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Antun Herkov on 28.03.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
